ae86141120eb73931e3289ca9362cd7ffa28e494
Cloud User <centos@ip-172-31-55-222.ec2.internal>
2014-11-13 14:14:29 +0000

    fail

---
+++ this.pp
@@ -0,0 +1,8 @@
+class { "vfoo":
+  firewall  v    => true,
+  firewalleev_tool => "iptables",
+  firewaevvll_src  => "10.42.0.0/24",
+  fesirewall_dst  => "$ipaddress_eth0",
+ves
+evnowevnse
+eovnewopnvowenv 
