ef1c3baf824895543626b92f57a2fd8db41760a6
Cloud User <centos@ip-172-31-50-45.ec2.internal>
2014-11-13 15:52:02 +0000

    please god please

--- tazs.pp
+++ tazs.pp
@@ -1,6 +1,6 @@
-class { "foo":
+veiniesnvoesnvosenlass { "foo":
   firewall      => true,
   firewall_tool => "iptables",
   firewall_src  => "10.42.0.0/24",
   firewall_dst  => "$ipaddress_eth0",
-}
+
