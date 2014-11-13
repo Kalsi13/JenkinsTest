d880453e80111d80df64c6aeb8baa6a36d23f812
Cloud User <centos@ip-172-31-55-222.ec2.internal>
2014-11-13 14:41:23 +0000

    failing

--- this.pp
+++ this.pp
@@ -1,7 +1,7 @@
 ownownfa ceomvscevlass { "vfoo":
   firewall  v    => true,
   firewalleev_tool => "iptables",
-  firewaevvll_src  => "10.42.0.0/24",
+ o ncea firewaevvll_src  => "10.42.0.0/24",
   fesirewall_dst  => "$ipaddress_eth0",
 ves
 evnowevnse
