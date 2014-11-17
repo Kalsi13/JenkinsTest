6c69dda593a67d50cecff75f1db834c04d08a3c7
Cloud User <centos@ip-172-31-17-19.us-west-2.compute.internal>
2014-11-17 10:30:20 +0000

    try fix

--- cunting.pp
+++ cunting.pp
@@ -1,6 +1,5 @@
-clavesss {v "foo":
-  firesevwvall      => true,
-  fievessrewall_tool => "iptables",
-  fvirewall_src  => "10.42.0.0/24",
- ev firewall_dst  => "$ipaddress_eth0",
-
+classes {v "foo":
+  firewall  => true,
+  firewall_tool => "iptables",
+  firewall_src  => "10.42.0.0/24",
+  firewall_dst  => "$ipaddress_eth0"
