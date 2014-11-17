ec8b4f28366d6428353f78d9f748f6593dc7d2ee
Kalsi13 <mandeep.kalsi@bskyb.com>
2014-11-17 10:34:56 +0000

    Delete this.pp

--- this.pp
+++
@@ -1,8 +0,0 @@
-ownownfa ceomvscevlass { "vfoo":
-  firewall  v    => true,
-  firewalleev_tool => "iptables",
- o ncea firewaevvll_src  => "10.42.0.0/24",
-  fesirewall_dst  => "$ipaddress_eth0",
-ves
-evnowevnse
-eovnewopnvowenv 
